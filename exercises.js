394ca66d6b9b57ab3f842a65fec1caa81a801c65
erickesouzaa <ericke.souzaa@gmail.com>
2025-11-29 07:45:55 -0300

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -1,29 +1,21 @@
 // exercises.js
-// Biblioteca de exercícios separada do app.js
-// Fica disponível em window.EXERCISE_LIBRARY
-
-window.EXERCISE_LIBRARY = [
+export const EXERCISE_LIBRARY = [
   { id: "supino_reto", name: "Supino Reto com Barra", group: "Peito" },
   { id: "supino_inclinado", name: "Supino Inclinado com Halteres", group: "Peito" },
   { id: "crucifixo_maquina", name: "Crucifixo no Voador", group: "Peito" },
   { id: "flexao", name: "Flexão de Braços", group: "Peito / Tríceps" },
-
   { id: "agachamento", name: "Agachamento Livre", group: "Pernas" },
   { id: "leg_press", name: "Leg Press 45°", group: "Pernas" },
   { id: "extensora", name: "Cadeira Extensora", group: "Quadríceps" },
   { id: "flexora", name: "Cadeira Flexora", group: "Posterior" },
   { id: "panturrilha", name: "Panturrilha em Pé", group: "Panturrilha" },
-
   { id: "remada_barra", name: "Remada Curvada com Barra", group: "Costas" },
   { id: "puxada_alta", name: "Puxada Alta", group: "Costas" },
   { id: "remada_baixa", name: "Remada Baixa", group: "Costas" },
-
   { id: "rosca_direta", name: "Rosca Direta", group: "Bíceps" },
   { id: "rosca_martelo", name: "Rosca Martelo", group: "Bíceps" },
-
   { id: "triceps_corda", name: "Tríceps Corda", group: "Tríceps" },
   { id: "triceps_testa", name: "Tríceps Testa", group: "Tríceps" },
-
   { id: "desenvolvimento", name: "Desenvolvimento com Halteres", group: "Ombros" },
   { id: "elevacao_lateral", name: "Elevação Lateral", group: "Ombros" },
 ];
